56d2f13f3b01ec1fb1bc9358c4a4123b48962051
Masood <57627236+masxxiii@users.noreply.github.com>
2022-02-13 21:05:33 +0700

    Update getSchema.ts

--- server/src/schemas/getSchema.ts
+++ server/src/schemas/getSchema.ts
@@ -1,10 +1,1 @@
-'use strict';
-const Joi = require('joi');
 
-/**
- * Exporting the schema for our get tokens response.
- */
-export const getData = Joi.array().items({
-    list:
-        Joi.string().required()
-});
