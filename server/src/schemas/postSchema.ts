8dd766bd227eb1baaa3ce4ac0c9cbf650d246c92
Masood <57627236+masxxiii@users.noreply.github.com>
2022-02-13 21:05:50 +0700

    Update postSchema.ts

--- server/src/schemas/postSchema.ts
+++ server/src/schemas/postSchema.ts
@@ -1,14 +1,1 @@
-'use strict';
-const Joi = require('joi');
 
-/**
- * Exporting the schema for our POST payload.
- */
-export const postData = Joi.object({
-    tokenAddress:
-        Joi.string().token().required(),
-    amount:
-        Joi.number().min(0).required(),
-    userAddress:
-        Joi.string().token().required(),
-});
